e43040a055d22521b98ec300e861513e8953cfa5
Alfonso Siregar <thermalblue17@gmai.com>
2022-10-05 18:05:01 +0700

    lomba creativity blog

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container:{
+      center:true,
+      padding: '16px',
+    },
+    extend: {
+      colors:{
+        primary: '#b45309',
+        secondary:'#d97706',
+        dark:'#78350f'
+      },
+      
+    screens: {
+      '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
